68afaf3de665859731409c20772857f55803b418
Rob Enriquez <robe.games.ios@gmail.com>
2024-02-11 22:51:39 -0800

    added music visualizer

--- PlexMatrixDisplay/src/PMDWebPage.h
+++ PlexMatrixDisplay/src/PMDWebPage.h
@@ -70,6 +70,12 @@ const char SETTINGS_PAGE[] PROGMEM = R""""(
     <a class="update-button" onclick="updatePreference('selectedTheme', 100)">Select</a>
 </div>
 <div class="card">
+    <h2 class="card-title">Plex Music Cover Art</h2>
+    <p class="card-description">Displays Plex Music Cover Art.</p>
+    <img class="card-image" src="https://github.com/robegamesios/clockwise/blob/0b6b92c9c09d9c3aae5ba50888654250d2410f3b/firmware/lib/clockfaces/thumbnails/audio_visualizer.jpg?raw=true" alt="Plex Music Cover Art">
+    <a class="update-button" onclick="updatePreference('selectedTheme', 200)">Select</a>
+</div>
+<div class="card">
     <h2 class="card-title">GIF Art</h2>
     <p class="card-description">Display your favorite GIF Art.</p>
     <img class="card-image" src="https://github.com/robegamesios/clockwise/blob/0b6b92c9c09d9c3aae5ba50888654250d2410f3b/firmware/lib/clockfaces/thumbnails/audio_visualizer.jpg?raw=true" alt="Gif Art">
