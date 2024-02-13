bac70b26614400ecae2d84b334c67bd7829ffb2c
Rob Enriquez <robe.games.ios@gmail.com>
2024-02-12 21:13:05 -0800

    update

--- PlexMatrixDisplay/src/main.cpp
+++ PlexMatrixDisplay/src/main.cpp
@@ -551,7 +551,6 @@ void downloadGifArt() {
   HTTPClient http;
 
   // Specify the URL of the GIF file on raw.githubusercontent.com
-  // String gifURL = "https://raw.githubusercontent.com/robegamesios/PlexMatrixDisplay/2e8e2f7d30f77fe394dd56a6a28c7c73e82e9d94/shared/gifs/bugcat-army.gif";
   String gifURL = "https://raw.githubusercontent.com/robegamesios/PlexMatrixDisplay/db29366cae2671a4acee1b9272338ad7edd941f6/shared/gifs/ezgif.com-pacmn.gif";
 
   // Start the HTTP request to download the GIF file
@@ -1457,7 +1456,7 @@ void setup()
 
   server.begin();
 
-  downloadGifArt();
+  // downloadGifArt();
 }
 
 void loop()
@@ -1466,15 +1465,15 @@ void loop()
   {
     handleHttpRequest();
 
-    // if (selectedTheme == AUDIO_VISUALIZER_THEME)
-    // {
-    //   loopAudioVisualizer();
-    // }
-    // else if (selectedTheme == PLEX_COVER_ART_THEME)
-    // {
-    //   getAlbumArt();
-    //   delay(5000); // Check every 5 seconds
-    // }
+    if (selectedTheme == AUDIO_VISUALIZER_THEME)
+    {
+      loopAudioVisualizer();
+    }
+    else if (selectedTheme == PLEX_COVER_ART_THEME)
+    {
+      getAlbumArt();
+      delay(5000); // Check every 5 seconds
+    }
   }
 }
 
